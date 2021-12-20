13edd0dc0771ba16d4da9c4d038d25f16ce77a0d
Fishroud <3334679655@qq.com>
2021-12-20 16:19:26 +0800

    优化图片编码，新增解析b站分享，优化代码

--- OlivaBilibiliPlugin/bilibili.py
+++ OlivaBilibiliPlugin/bilibili.py
@@ -100,16 +100,6 @@ class BILIUSER:
         if userjson['code'] == 0:
             self.live_key_frame = userjson['data'][self.mid]['keyframe']
 
-
-
-    def __str__(self):
-        if self.isLegal():
-            output = self.name + "[" + self.sex + "]" + "[LV" + str(self.level) + "]\n"
-            output += self.sign
-            return output
-        else:
-            return "用户不存在"
-    
     def isLegal(self):
         return self.islegal
     
@@ -158,7 +148,7 @@ class BILIUSER:
             output += self.name + "[" + str(self.mid) + "]" + "\n"
             output += "https://live.bilibili.com/" + str(self.live['roomid']) + "\n"
             output += "标题：" + self.live['title'] + "\n"
-            output += "[CQ:image,file=" + self.live['cover'] + "]\n"
+            output += "[OP:image,file=" + self.live['cover'] + "]\n"
             return output
         else:
             return "用户不存在"
@@ -176,6 +166,7 @@ class VIDEO:
         self.pubdate = None
         self.ctime = None
         self.desc = None
+        self.nowWatching = None
         self.owner = {}
         self.state = {}
         self.first_frame = None
@@ -206,38 +197,38 @@ class VIDEO:
             self.state = videojson['data']['stat']
             #self.first_frame = videojson['data']['pages'][0]['first_frame']
             self.islegal = True
+        api = "https://api.bilibili.com/x/player/online/total?cid="+ str(self.cid) + "&bvid=" + str(self.bvid)
+        payload={}
+        headers = {
+          'Cookie': 'LIVE_BUVID=AUTO2616196871676659'
+        }
+        response = requests.request("GET", api, headers=headers, data=payload)
+        responsejson = json.loads(response.text)
+        if responsejson['code'] == 0:
+            self.nowWatching = responsejson['data']['total']
+        return
 
-    def __str__(self):
-        if self.isLegal():
-            output = "[" + self.bvid + "]" + "[" + str(self.cid) + "]" + "[" + str(self.aid) + "]" + "\n"
-            output += "标题：" + self.title + "\n"
-            output += "分区：" + self.tname + "\n"
-            output += "封面链接：" + self.pic + "\n"
-            output += "发布时间：" + str(self.pubdate) + "\n"
-            output += "投稿时间：" + str(self.ctime) + "\n"
-            output += "简介：" + self.desc + "\n"
-            output += "点赞投币收藏：" + str(self.state['like']) + "-" + str(self.state['coin']) + "-" + str(self.state['favorite']) + "\n"
-            output += "分区：" + self.tname + "\n"
-            pic = LoadImg(self.pic)
-            pic.show()
-            return output
-        else:
-            return "视频信息加载失败"
 
     def isLegal(self):
         return self.islegal
-    
+
     def getVideoInfo(self):
         if self.isLegal():
             output = "[" + self.bvid + "]" + "\n" #"[" + str(self.cid) + "]" + "[" + str(self.aid) + "]" + 
-            output += "[CQ:image,file=" + self.pic + "]\n"
+            output += "[OP:image,file=" + self.pic + "]\n"
             output += "标题：" + self.title + "\n"
+            output += "UP主：" + self.owner['name'] + "[" + str(self.owner['mid']) + "]\n"
             output += "分区：" + self.tname + "\n"
             output += "发布时间：" + time.strftime("%Y-%m-%d %H:%M", time.localtime(self.pubdate)) + "\n"
             output += "投稿时间：" + time.strftime("%Y-%m-%d %H:%M", time.localtime(self.ctime)) + "\n"
-            output += "简介：" + self.desc + "\n"
+            if self.desc == "":
+                output += "简介： - \n"
+            else:
+                output += "简介：" + self.desc + "\n"
             output += "点赞投币收藏：" + str(self.state['like']) + "-" + str(self.state['coin']) + "-" + str(self.state['favorite']) + "\n"
-            output += "播放数量：" + str(self.state['view'])
+            output += "播放数量：" + str(self.state['view']) + "\n"
+            output += "在线观看人数：" + str(self.nowWatching) + "\n"
+            output += "链接：https://www.bilibili.com/video/"+ self.bvid
             return output
         else:
             return "视频查询失败"
@@ -280,6 +271,13 @@ class URL:
     def path2list(self):
         path_list = list(filter(None,self.path.split("/")))
         return path_list
+    
+    def getHtml(self):
+        url = self.url
+        payload={}
+        headers = {}
+        response = requests.request("GET", url, headers=headers, data=payload, allow_redirects=False)
+        return response.text
 
 
 def searchUserByName(name):
@@ -300,7 +298,7 @@ def searchUserByName(name):
             output += "由于条数过多，将优先显示前五条\n"
             search_result_list = search_result_list[:5]
         for search_result_this in search_result_list:
-            output += "[CQ:image,file=https:" + search_result_this['upic'] + "]\n"
+            output += "[OP:image,file=https:" + search_result_this['upic'] + "]\n"
             output += search_result_this['uname'] + "[" + str(search_result_this['mid']) + "][LV" + str(search_result_this['level']) + "]\n"
             if search_result_this['usign'] == "":
                 output += "此用户没有个性签名啊( `д´)\n"
--- OlivaBilibiliPlugin/msgReply.py
+++ OlivaBilibiliPlugin/msgReply.py
@@ -15,100 +15,113 @@ import OlivaBilibiliPlugin
 
 import os
 import re
+import json
+import requests
 
 def unity_reply(plugin_event, Proc):
 
 
     command_list = deleteBlank(plugin_event.data.message)
-    matchBV = re.match( r'^.*BV(\S{10}).*$', command_list[0], re.I)
-    matchUrl = re.match( r'(https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]', command_list[0])
-    image_path = os.path.abspath(OlivaBilibiliPlugin.data.save_path).replace("\\","\\\\")
+    image_path = os.path.abspath(OlivaBilibiliPlugin.data.save_path).replace('\\','\\\\')
+    matchJson = re.search(r'\[OP:json,data=(.*)\]', plugin_event.data.message)
 
+    if matchJson:
+        opjson = json.loads(matchJson.group(1).replace('&#44;',','))
+        url = opjson['meta']['detail_1']['qqdocurl']
+        if url:
+            url = OlivaBilibiliPlugin.bilibili.URL(url)
+            if url.netloc == 'b23.tv':
+                html = url.getHtml().replace('\n','')
+                matchbv = matchBV(html)
+                if matchbv:
+                    bvid = matchbv.group(1)
+                    video = OlivaBilibiliPlugin.bilibili.VIDEO(bvid)
+                    video.getVideoDataFromApi()
+                    response = video.getVideoInfo()
+                    if response != '视频查询失败':
+                        plugin_event.reply(response)
+                del url
     if len(command_list) == 1:
-        if command_list[0].lower() == "/bilibili":
-            plugin_event.reply("OlivaBilibiliPlugin by Fishroud")
-        elif matchBV:
-            bvid = matchBV.group(1)
+        matchbv = matchBV(command_list[0])
+        matchurl = matchUrl(command_list[0])
+        if command_list[0].lower() == '/bilibili':
+            plugin_event.reply('OlivaBilibiliPlugin by Fishroud')
+        elif matchbv:
+            bvid = matchbv.group(1)
             video = OlivaBilibiliPlugin.bilibili.VIDEO(bvid)
             video.getVideoDataFromApi()
             response = video.getVideoInfo()
-            if response != "视频查询失败":
+            if response != '视频查询失败':
                 plugin_event.reply(response)
-        elif matchUrl:
-            url = OlivaBilibiliPlugin.bilibili.URL(matchUrl.group(0))
-            if url.netloc == "live.bilibili.com":
+        elif matchurl:
+            url = OlivaBilibiliPlugin.bilibili.URL(matchurl.group(0))
+            if url.netloc == 'live.bilibili.com':
                 if url.path_list[0].isdigit():
                     biliUser = OlivaBilibiliPlugin.bilibili.BILIUSER()
                     biliUser.getUserDatabyRoomId(int(url.path_list[0]))
                     biliUser.getUserDatafromApi()
                     response = biliUser.getLiveInfo()
-                    if response != "用户不存在":
-                        #save_path = image_path + "\\" + str(biliUser.mid) + ".PNG"
-                        #cqcode = "[CQ:image,file=file:///" + save_path + "]"
-                        plugin_event.reply("发现了bilibili直播房间链接！\n" + response)
+                    if response != '用户不存在':
+                        #save_path = image_path + '\\' + str(biliUser.mid) + '.PNG'
+                        #cqcode = '[OP:image,file=file:///' + save_path + ']'
+                        plugin_event.reply('发现了bilibili直播房间链接！\n' + response)
                         del url,biliUser
-            elif url.netloc == "space.bilibili.com":
+            elif url.netloc == 'space.bilibili.com':
                 if url.path_list[0].isdigit():
                     biliUser = OlivaBilibiliPlugin.bilibili.BILIUSER(int(url.path_list[0]))
                     response = biliUser.getUserInfo()
-                    if response != "用户不存在":
-                        save_path = image_path + "\\" + str(biliUser.mid) + ".PNG"
-                        cqcode = "[CQ:image,file=file:///" + save_path + "]"
+                    if response != '用户不存在':
+                        save_path = image_path + '\\' + str(biliUser.mid) + '.PNG'
+                        cqcode = '[OP:image,file=file:///' + save_path + ']'
                         plugin_event.reply(response + cqcode)
                         del url,biliUser
-            elif url.netloc == "b23.tv":
-                pass
-
     if len(command_list) == 2:
-        if command_list[0].lower() == "/search":
+        if command_list[0].lower() == '/search':
             response = OlivaBilibiliPlugin.bilibili.searchUserByName(command_list[1])
             plugin_event.reply(response)
-
-
-
     if len(command_list) == 3:
-        if command_list[0].lower() == "/up":
+        if command_list[0].lower() == '/up':
           #command_list[2].isdigit():
-            if command_list[1].lower() == "--uid" or command_list[1].lower() == "-u":
+            if command_list[1].lower() == '--uid' or command_list[1].lower() == '-u':
                 if command_list[2].isdigit():
                     biliUser = OlivaBilibiliPlugin.bilibili.BILIUSER(int(command_list[2]))
-                    save_path = image_path + "\\" + str(biliUser.mid) + ".PNG"
-                    cqcode = "[CQ:image,file=file:///" + save_path + "]"
+                    save_path = image_path + '\\' + str(biliUser.mid) + '.PNG'
+                    cqcode = '[OP:image,file=file:///' + save_path + ']'
                     plugin_event.reply(biliUser.getUserInfo() + cqcode)
                 else:
-                    plugin_event.reply("[--uid]的参数非法")
-            elif command_list[1].lower() == "--roomid" or command_list[1].lower() == "-r":
+                    plugin_event.reply('[--uid]的参数非法')
+            elif command_list[1].lower() == '--roomid' or command_list[1].lower() == '-r':
                 if command_list[2].isdigit():
                     biliUser = OlivaBilibiliPlugin.bilibili.BILIUSER()
                     biliUser.getUserDatabyRoomId(int(command_list[2]))
                     biliUser.getUserDatafromApi()
-                    save_path = image_path + "\\" + str(biliUser.mid) + ".PNG"
-                    cqcode = "[CQ:image,file=file:///" + save_path + "]"
+                    save_path = image_path + '\\' + str(biliUser.mid) + '.PNG'
+                    cqcode = '[OP:image,file=file:///' + save_path + ']'
                     plugin_event.reply(biliUser.getUserInfo() + cqcode)
                 else:
-                    plugin_event.reply("[--roomid]的参数非法")
-        elif command_list[0].lower() == "/video":
+                    plugin_event.reply('[--roomid]的参数非法')
+        elif command_list[0].lower() == '/video':
             if len(command_list) == 3:  #command_list[2].isdigit():
-                if command_list[1].lower() == "--aid" or command_list[1].lower() == "-a":
+                if command_list[1].lower() == '--aid' or command_list[1].lower() == '-a':
                     if command_list[2].isdigit():
                         video = OlivaBilibiliPlugin.bilibili.VIDEO(0 ,int(command_list[2]))
-                        video.getVideoDataFromApi("aid")
+                        video.getVideoDataFromApi('aid')
                         plugin_event.reply(video.getVideoInfo())
                     else:
-                        plugin_event.reply("[--aid]的参数非法")
-                elif command_list[1].lower() == "--bvid" or command_list[1].lower() == "-b":
+                        plugin_event.reply('[--aid]的参数非法')
+                elif command_list[1].lower() == '--bvid' or command_list[1].lower() == '-b':
                     video = OlivaBilibiliPlugin.bilibili.VIDEO(command_list[2])
                     video.getVideoDataFromApi()
                     plugin_event.reply(video.getVideoInfo())
 
-
-
-
-
-
-
-
-
 def deleteBlank(str):
-    str_list = list(filter(None,str.split(" ")))
+    str_list = list(filter(None,str.split(' ')))
     return str_list
+
+def matchBV(str):
+    matchbv = re.match( r'^.*BV(\S{10}).*$', str, re.I)
+    return matchbv
+
+def matchUrl(str):
+    matchurl = re.match( r'(https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]', str)
+    return matchurl
